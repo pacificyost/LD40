663803106597b3a85a0ac6abd59a80c03e17abbd
pacificyost <pacificyost@gmail.com>
2017-12-01 19:57:00 -0800

    Basic Navmesh utility
    Added a basic plane and capsule and using a navmesh to move the player
    to points within reachable space.

---
+++ LD40/Assets/Scripts/MouseInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MouseInput : MonoBehaviour {
+
+    public Camera mainCamera;
+    int mouseDistance = 100;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        //mainCamera = Camera.current;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        RaycastHit hit;
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray mouseRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+            Debug.Log(mouseRay);
+            Physics.Raycast(mouseRay, out hit, mouseDistance);
+
+            if (player != null)
+            {
+                player.GetComponent<NavMeshAgent>().SetDestination(hit.point);
+            }
+        }
+	}
+}
