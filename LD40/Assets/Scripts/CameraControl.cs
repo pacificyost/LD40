b709ec1a1d6d72c41439af3d0e1085862808731f
pacificyost <pacificyost@gmail.com>
2017-12-02 12:42:50 -0800

    Camera control script
    Add camera control script to move the camera with the player object
    without rotation.

---
+++ LD40/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    GameObject player;
+    Vector3 cameraOffset;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        cameraOffset = new Vector3(0, 30, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + cameraOffset;
+	}
+}
