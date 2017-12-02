132a099dc0fa4af4827a9d9e6fccb4b86e98e49b
pacificyost <pacificyost@gmail.com>
2017-12-01 22:47:41 -0800

    Collection script update
    Collect after delay and only if still within range

--- LD40/Assets/Scripts/Collect.cs
+++ LD40/Assets/Scripts/Collect.cs
@@ -6,8 +6,10 @@ public class Collect : MonoBehaviour {
 
     public GameObject goal;
     public float collectDistance = 1.0f;
-    Transform self;
+    private Transform self;
     public int collected = 0;
+    public float collectDelay = 10.0f;
+    public bool collecting = false;
 
 	// Use this for initialization
 	void Start () {
@@ -17,20 +19,40 @@ public class Collect : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        if (WithinRange() && collecting == false)
+        {
+            Invoke("CollectIt",collectDelay);
+            collecting = true;
+        }
+
+        if (WithinRange() == false)
+        {
+            collecting = false;
+        }
+
+	}
+
+    private void CollectIt()
+    {
+        if (WithinRange() && collecting == true)
+        {
+            collected++;
+            collecting = false;
+        }
+    }
+
+    private bool WithinRange()
+    {
         if (goal != null)
         {
             Vector3 directionToGoal = goal.transform.position - self.position;
 
             if (directionToGoal.magnitude < collectDistance)
             {
-                CollectIt(goal);
 
+                return true;
             }
         }
-	}
-
-    private void CollectIt(GameObject collectible)
-    {
-        collected++;
+        return false;
     }
 }
