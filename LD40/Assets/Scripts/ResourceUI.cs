e6204783336bb2aabc6089bc3653a7046b3a98fc
pacificyost <pacificyost@gmail.com>
2017-12-02 12:58:54 -0800

    Basic UI
    Added basic UI element to show resource count

---
+++ LD40/Assets/Scripts/ResourceUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceUI : MonoBehaviour {
+    Inventory inventory;
+    Text resourcesText;
+
+	// Use this for initialization
+	void Start () {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        resourcesText = GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        resourcesText.text = inventory.collected.ToString();
+        
+	}
+}
