998fc2649da6f521bf4bda5dfe980dc64215a8e8
pacificyost <pacificyost@gmail.com>
2017-12-03 15:07:55 -0800

    Enemy Nests spawn enemies
    Enemies now spawn from nests and consume their resources to spawn.  The
    nest recharges over time unless it is exhausted.

---
+++ LD40/Assets/Scripts/NestAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NestAI : MonoBehaviour {
+    public float spawnDelay = 10.0f;
+    public GameObject spawnPrefab;
+    public float rechargeDelay = 20.0f;
+    public int rechargeAmount = 1;
+
+    Inventory inventory;
+	// Use this for initialization
+	void Start () {
+        inventory = GetComponent<Inventory>();
+        InvokeRepeating("Spawn", spawnDelay, spawnDelay);
+        InvokeRepeating("Recharge",rechargeDelay,rechargeDelay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void Spawn()
+    {
+        if (spawnPrefab != null && inventory.quantity > 0)
+        {
+            GameObject newlySpawned = Instantiate(spawnPrefab, transform.position, Quaternion.identity);
+            Debug.Log(newlySpawned.transform.position);
+            inventory.quantity--;
+        }
+    }
+
+    private void Recharge()
+    {
+        inventory.quantity += rechargeAmount;
+    }
+}
