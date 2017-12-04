de8bb7cc1f2e3895eefa5789b8715341af5965f1
pacificyost <pacificyost@gmail.com>
2017-12-03 17:31:31 -0800

    Final Changes for Compo
    Final chanes for Compo submission.  New title and endgame scenes.

---
+++ LD40/Assets/Scripts/Title.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("Game", 5);
+	}
+	
+	void Game()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
